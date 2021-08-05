ecdccc35b0510a972cc98864801bf625654113c2
Kuan-Wen Chen <kev888kev@gmail.com>
2021-08-05 21:49:58 +0800

    [Balanced BST] AVL tree deletion

--- Tree and Graph/binary tree/balanced tree/AVLTree.cpp
+++ Tree and Graph/binary tree/balanced tree/AVLTree.cpp
@@ -131,6 +131,7 @@ Node *insert(Node *node, int key)
     /* 3. Get the balance factor of this ancestor
 		node to check whether this node became
 		unbalanced */
+
     int balance = getBalance(node);
 
     // If this node becomes unbalanced, then
@@ -162,6 +163,123 @@ Node *insert(Node *node, int key)
     return node;
 }
 
+/* Given a non-empty binary search tree,
+return the node with minimum key value
+found in that tree. Note that the entire
+tree does not need to be searched. */
+Node *minValueNode(Node *node)
+{
+    Node *current = node;
+
+    /* loop down to find the leftmost leaf */
+    while (current->left != nullptr)
+        current = current->left;
+
+    return current;
+}
+
+Node *deleteNode(Node *root, int key)
+{
+
+    // STEP 1: PERFORM STANDARD BST DELETE
+    if (root == nullptr)
+        return root;
+
+    // If the key to be deleted is smaller
+    // than the root's key, then it lies
+    // in left subtree
+    if (key < root->key)
+        root->left = deleteNode(root->left, key);
+
+    // If the key to be deleted is greater
+    // than the root's key, then it lies
+    // in right subtree
+    else if (key > root->key)
+        root->right = deleteNode(root->right, key);
+
+    // if key is same as root's key, then
+    // This is the node to be deleted
+    else
+    {
+        // node with only one child or no child
+        if ((root->left == nullptr) ||
+            (root->right == nullptr))
+        {
+            Node *temp = root->left ? root->left : root->right;
+
+            // No child case
+            if (temp == nullptr)
+            {
+                temp = root;
+                root = nullptr;
+            }
+            else               // One child case
+                *root = *temp; // Copy the contents of
+                               // the non-empty child
+            delete temp;
+        }
+        else
+        {
+            // node with two children: Get the inorder
+            // successor (smallest in the right subtree)
+            Node *temp = minValueNode(root->right);
+
+            // Copy the inorder successor's
+            // data to this node
+            root->key = temp->key;
+
+            // Delete the inorder successor
+            root->right = deleteNode(root->right,
+                                     temp->key);
+        }
+    }
+
+    // If the tree had only one node
+    // then return
+    if (root == nullptr)
+        return root;
+
+    // STEP 2: UPDATE HEIGHT OF THE CURRENT NODE
+    root->height = 1 + max(height(root->left),
+                           height(root->right));
+
+    // STEP 3: GET THE BALANCE FACTOR OF
+    // THIS NODE (to check whether this
+    // node became unbalanced)
+    int balance = getBalance(root);
+
+    // If this node becomes unbalanced,
+    // then there are 4 cases
+
+    // Left Left Case
+    if (balance > 1 &&
+        getBalance(root->left) >= 0)
+        return rightRotate(root);
+
+    // Left Right Case
+    if (balance > 1 &&
+        getBalance(root->left) < 0)
+    {
+        root->left = leftRotate(root->left);
+        return rightRotate(root);
+    }
+
+    // Right Right Case
+    if (balance < -1 &&
+        getBalance(root->right) <= 0)
+        return leftRotate(root);
+
+    // Right Left Case
+    if (balance < -1 &&
+        getBalance(root->right) > 0)
+    {
+        root->right = rightRotate(root->right);
+        return leftRotate(root);
+    }
+
+    return root;
+}
+
 void preOrder(Node *root)
 {
     if (root != nullptr)
@@ -175,25 +293,47 @@ void preOrder(Node *root)
 int main()
 {
     Node *root = nullptr;
-
     /* Constructing tree given in
-	the above figure */
+    the above figure */
+    root = insert(root, 9);
+    root = insert(root, 5);
     root = insert(root, 10);
-    root = insert(root, 20);
-    root = insert(root, 30);
-    root = insert(root, 40);
-    root = insert(root, 50);
-    root = insert(root, 25);
+    root = insert(root, 0);
+    root = insert(root, 6);
+    root = insert(root, 11);
+    root = insert(root, -1);
+    root = insert(root, 1);
+    root = insert(root, 2);
 
     /* The constructed AVL Tree would be
-		     30
-			/ \
-		   20  40
-		  / \    \
-		10  25    50
-	*/
+            9
+           / \
+          1  10
+         / \  \
+        0  5  11
+        / / \
+      -1 2   6
+    
+    */
+
     cout << "Preorder traversal of the "
             "constructed AVL tree is \n";
+    preOrder(root);
+
+    root = deleteNode(root, 10);
+
+    /* The AVL Tree after deletion of 10
+            1
+           / \
+          0   9
+         /   / \
+       -1   5   11
+           / \
+          2   6
+    */
+
+    cout << "\nPreorder traversal after"
+         << " deletion of 10 \n";
     preOrder(root);
 
     return 0;
